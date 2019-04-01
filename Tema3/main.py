29773f0b21b133fab04105b1fad205184e20d0f7
Schitcu Gabriel <gabriel_schitcu@yahoo.com>
2019-04-01 23:13:19 +0300

    Progress

--- Tema3/main.py
+++ Tema3/main.py
@@ -38,7 +38,7 @@ def getPlaceDetails(location):
     url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyCNQX5-4_hPDpluC7j-EZK13Oixn_47DpM&input=' + location + '&inputtype=textquery'
     response = requests.get(url)
     if response.ok:
-        placeId = response.json()['place_id']
+        placeId = response.json()['candidates'][0]['place_id']
         url = 'https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyCNQX5-4_hPDpluC7j-EZK13Oixn_47DpM&placeid=' + placeId
         response = requests.get(url)
         if response.ok:
