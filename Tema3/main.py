db05bc579fd692aba3990706a2d3b33028ed4e74
Schitcu Gabriel <gabriel_schitcu@yahoo.com>
2019-04-01 20:26:21 +0300

    Progress

--- Tema3/main.py
+++ Tema3/main.py
@@ -177,7 +177,7 @@ def insertInDatastore():
 @app.route('/compute', methods=['GET', 'POST'])
 def compute():
     input = request.args.get('query')
-    print(getFromDatastore(input))
+    print(getFromDatastore(input)['timestamp'])
 
     #datastore_client.put(entity)
     mapImage = getImageFromlocation(input)
